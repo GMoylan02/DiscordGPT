2dcc7c48b968a5a3d13694d8fcd48e0f4d978942
GMoylan02 <gerardmoylan02@gmail.com>
2024-01-28 15:11:34 +0000

    Update responses.py

--- src/responses.py
+++ src/responses.py
@@ -2,13 +2,14 @@ from langchain_core.messages import SystemMessage
 from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
+from langchain.schema import HumanMessage, SystemMessage
 import dotenv
 
 dotenv.load_dotenv()
 
 chat = ChatOpenAI()
 conversation = ConversationChain(llm=chat)
-system_message = SystemMessage(content="You are a funny discord chatbot who provides humorous responses during a "
+system_message = SystemMessage(content="You are a funny Discord chat bot who provides humorous responses during a "
                                        "conversation.")
 
 
@@ -16,7 +17,7 @@ def get_response(user_input: str) -> str:
     if user_input == '':
         return 'User input not found'
 
-    return conversation.run([system_message, user_input])
+    return chat([system_message, HumanMessage(content=user_input)]).content
 
 
 
